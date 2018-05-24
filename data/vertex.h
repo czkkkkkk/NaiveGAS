0f212026ec8728776b1f78c2d5e7721e82d9d1ee
czkkkkkk <zekucai@gmail.com>
2018-05-24 11:00:02 +0800

    [vertex] add base function

--- data/vertex.h
+++ data/vertex.h
@@ -1,13 +1,19 @@
 #pragma once
 
+#include <vector>
+
 #include <data/macros.h>
 
 namespace NGAS {
 
-class Vertex {
+template <typename MsgT> class Vertex {
 public:
+  virtual void Combine(MsgT &lhs, const MsgT &rhs) = 0;
+  virtual void Apply(const MsgT &msg) = 0;
+  virtual std::vector<std::pair<IdType, MsgT>> Scatter() = 0;
+
 private:
-  IdType src, dst;
+  IdType id_;
 };
 
 } // namespace NGAS
