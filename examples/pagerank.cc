75b3e61c6e9d77595506594d3cfeb3bef8a7d1ff
czkkkkkk <zekucai@gmail.com>
2018-05-26 14:34:54 +0800

    [data] Modify vertex api

--- examples/pagerank.cc
+++ examples/pagerank.cc
@@ -1,14 +1,22 @@
+#include "data/edge.h"
 #include "data/vertex.h"
 #include "engine/execute.h"
 
 using namespace NGAS;
 
-class PRVertex : public Vertex<double> {
+class PREdge : public Edge {};
+
+class PRVertex : public Vertex<double, PREdge> {
 public:
   using MsgT = double;
+  using EdgeT = PREdge;
+
   void Combine(double &lhs, const double &rhs) override { lhs += rhs; }
   void Apply(const double &msg) override { pr_value_ = msg; }
-  std::vector<std::pair<IdType, double>> Scatter() override { return {}; }
+  std::vector<std::pair<IdType, double>>
+  Scatter(const EdgeT &edge) const override {
+    return {};
+  }
 
 private:
   double pr_value_;
